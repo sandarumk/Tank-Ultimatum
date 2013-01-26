462d57474c4677a1d494e40c542792d7d2c79c99
Dinu Kumarasiri <sandarumk@gmail.com>
2013-01-26 16:40:55 +0530

    Update GameTanks/src/gametanks/Connection.java

--- GameTanks/src/gametanks/Connection.java
+++ GameTanks/src/gametanks/Connection.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package gametanks;
 
 import java.io.BufferedReader;
@@ -12,25 +9,47 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
-
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Connection {
+
     private BufferedWriter write;
     private BufferedReader read;
+    private ServerSocket clientserversoc;
 
-    public void joingame() throws UnknownHostException, IOException{
-Socket serversoc = new Socket("127.0.0.1", 6000);
+    public void joingame() throws UnknownHostException, IOException {
+        Socket serversoc = new Socket("127.0.0.1", 6000);
+        Timer timer = new Timer();
 
-write = new BufferedWriter(new OutputStreamWriter(serversoc.getOutputStream()));
-write.write("JOIN#");
-write.close();
+        write = new BufferedWriter(new OutputStreamWriter(serversoc.getOutputStream()));
+        write.write("JOIN#");
+        write.close();
+
+        clientserversoc = new ServerSocket(7000);
+
+        timer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                try {
+                    Socket clientsoc = clientserversoc.accept();
+                    read = new BufferedReader(new InputStreamReader(clientsoc.getInputStream()));
+                    String s = read.readLine();
+
+                    System.out.println(s);
+                    read.close();;
+
+                } catch (IOException ex) {
+                    Logger.getLogger(Connection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }, 0, 100);
 
 
-ServerSocket clientserversoc = new ServerSocket(7000);
-Socket clientsoc = clientserversoc.accept();
-read = new BufferedReader(new InputStreamReader(clientsoc.getInputStream()));
-System.out.println(read.readLine()); 
-// send msgs according to the line we get. 
-read.close();
-}
+
+
+    }
 }
